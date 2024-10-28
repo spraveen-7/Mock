483c1c2f24dc2ce39ef16ff76939307432bfeb7b
Praveen <Praveen@gmail.com>
2024-10-28 17:42:36 +0530

    push

--- Practice.java
+++ Practice.java
@@ -2,5 +2,7 @@ class Practice{
     public static void main(String[] args) {
         System.out.println("creating pull request");
         System.out.println("Branch1");
+        System.out.println("main");
     }
+
 }
